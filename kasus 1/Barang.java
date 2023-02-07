9064ff68c2d3813992d6c8c89d24663761895877
hanifzndn <117618640+hanifzndn@users.noreply.github.com>
2023-02-07 09:33:08 +0700

    Add files via upload

---
+++ kasus 1/Barang.java
@@ -0,0 +1,26 @@
+package com.mycompany.pertemuan_3;
+
+/**
+ *
+ * @author muhammad hanif
+ */
+public class Barang {
+    String kode_barang;
+    String nama_barang;
+    private int stok; 
+    
+    public Barang(String kode, String nama, int stk){
+         kode_barang = kode;
+         nama_barang = nama;
+         stok = stk;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+    
+}
---
+++ kasus 2/Item.java
@@ -0,0 +1,11 @@
+package com.mycompany.pertemuan_3;
+    public class Item {
+        private String name;
+        private Item() {
+            name = "Ipin";
+        }
+        public Item(String name) {
+            this();
+            System.out.println(this.name);
+}
+}
